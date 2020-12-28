a32c49ed62ad8dc81db6e985d77277617c224fb9
wahan9501 <wahan9501@gmail.com>
2020-12-28 23:06:26 +0800

    add clean, install scripts

---
+++ clean.sh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+echo "Dotfile clean start"
+
+stow -D zsh
+
+echo "Dotfile clean finished"
+echo :P
---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+echo "Dotfile install start"
+
+stow zsh
+
+echo "Dotfile install finished"
+echo :P
