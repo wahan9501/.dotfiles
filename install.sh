97389420978b9f564ec47ffb6e98eed3cfcdc7c6
han <wahan9501@gmail.com>
2021-01-04 12:46:02 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
-#!/bin/zsh
+#!/bin/bash
 echo "Dotfile install start..."
+
+sudo apt update
+sudo apt install zsh
+sudo apt install stow
+sudo apt install neovim
 
 git submodule init
 git submodule update
@@ -7,5 +12,8 @@ git submodule update
 stow zsh
 stow config
 
+sudo chsh -s $(which zsh)
 echo "Dotfile install finished..."
 echo :P
+
+exec zsh
