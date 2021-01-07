fa31400d3b0483ba625aae4d417963ec5bfec47b
han <wahan9501@gmail.com>
2021-01-07 15:18:15 +0800

    update install.sh     add navi install     add essential packages update wt config

--- install.sh
+++ install.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
 echo "Dotfile install start..."
 
-sudo pacman -Syu  zsh stow neovim --noconfirm
+install_navi()
+{
+    if [ -z `which navi` ]
+    then
+        temp_dir=/tmp/$USER
+        mkdir -p $temp_dir
+        git clone https://aur.archlinux.org/navi.git $temp_dir/navi
+
+        pushd $temp_dir/navi
+        makepkg -si
+        popd
+    else
+        echo "navi installed"
+    fi
+}
+
+sudo pacman -Syu  zsh --noconfirm
+sudo pacman -Syu  stow --noconfirm
+sudo pacman -Syu  neovim --noconfirm
+sudo pacman -Syu  fzf --noconfirm
+sudo pacman -Syu  neofetch --noconfirm
+install_navi
 
 git submodule init
 git submodule update
@@ -13,4 +34,5 @@ sudo chsh -s $(which zsh)
 echo "Dotfile install finished..."
 echo :P
 
-exec zsh
+clear && exec zsh
+
